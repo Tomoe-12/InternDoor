6950bb059eabcfee8557fb187ee2e6ba3edc3912
Tomoe-12 <li8993han@gmail.com>
2025-12-11 10:47:15 +0630

    Update database port to 3307 and fix logo text in frontend

--- frontend/components/logo.tsx
+++ frontend/components/logo.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export default function Logo() {
   return (
     <Link href={"/"}>
-      <h1 className="text-2xl font-bold bg-gradient-to-r from-primary-4 to-primary-9 inline-block text-transparent bg-clip-text">app.name</h1>
+      <h1 className="text-2xl font-bold bg-gradient-to-r from-primary-4 to-primary-9 inline-block text-transparent bg-clip-text">InternDoor</h1>
     </Link>
   )
 }
